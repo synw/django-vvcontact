855865fe33791c43488f5a05e9fc285e23ca08c5
Win <synwe@yahoo.com>
2017-03-30 12:59:20 +0000

    Corrections

--- vvcontact/templates/vvcontact/vues/methods.js
+++ vvcontact/templates/vvcontact/vues/methods.js
@@ -1,6 +1,7 @@
 {% load i18n %}
 loadContactForm: function() {
 	action = function(data) {
+		app.flush();
 		app.contactForm = data;
 		app.activate("contact");
 		document.title = "{% trans 'Contact' %}";
